c57074ea36ae72549c59c7f1539b77b0fe1bd102
Ethen·James·Freecss <onegold@live.cn>
2014-11-21 02:34:27 +1100

    Create yahtzee.h

---
+++ yahtzee.h
@@ -0,0 +1,12 @@
+/************************************************************************************* 
+* Filename: yahtzee.h * 
+* Student's first name: Yi  * 
+* Modification Date: 14/05/2013 *
+* Purpose of this file (or program): produce a program for playing the game Yahtzee.*
+*************************************************************************************/
+void enterseed();
+
+void rolling();
+
+void rules();
+
